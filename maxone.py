a0b7d3ce153f8c147d38f1c9a34a116300624e13
WhiteShadow13 <ludo.m@live.fr>
2020-05-03 10:57:46 +0200

    init

---
+++ maxone.py
@@ -0,0 +1,34 @@
+# maxone.py
+# Author: Sébastien Combéfis
+# Version: April 26, 2020
+
+IND_SIZE = 10
+POP_SIZE = 6
+
+# Initialising the population.
+population = []
+
+# Defining the fitness function.
+def evaluate(ind):
+    pass
+
+# Defining the mating function.
+def mate(ind1, ind2):
+    return ind1, ind2
+
+# Defining the mutation function.
+def mutate(ind):
+    return ind
+
+# Defining the selection function.
+def select(pop):
+    return pop
+
+
+# Running the simulation.
+if __name__ == '__main__':
+    PROB_MATING = 0.5
+    PROB_MUTATION = 0.2
+    ITERATIONS = 100
+
+    pass
